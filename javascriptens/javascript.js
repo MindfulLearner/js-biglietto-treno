b7b59f11ed88f6b55189e4fa48baed21cd7629e3
MindfulLearner <dimajosh1009@gmail.com>
2024-07-11 22:38:13 +0200

    aggiunti commenti

---
+++ javascriptens/javascript.js
@@ -0,0 +1,24 @@
+// JS JAVA SCROPTS
+
+console.log('ciao')
+// inserimento su prompt dei dati del passeggero
+
+// stiamo creando una struct 
+let Passeggero = {
+	percorsoKm: 0,
+	eta: "" 
+};
+
+// stampiamo per debuggare un po 
+console.log(Passeggero.percorsoKm);
+console.log(Passeggero.eta);
+// tipologia di informazione
+console.log(typeof(Passeggero.percorsoKm));
+console.log(typeof(Passeggero.eta));
+
+
+
+
+
+
+
